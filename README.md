d8f2f1ac7f3e1aa939b6d8092d9ed62afb2d8e02
Frank Burkitt <fburkitt@burkitt.com>
2016-05-29 18:48:44 -0700

    Refactored to add Blueprints and Fix a number of Issues

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a program that uses Flask to create a task management system
--- project/_config.py
+++ project/_config.py
@@ -7,9 +7,6 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# the WTF config setting is used for cross-suite request forgery prevention
-# The SECRET_KEY is used with WTF to create a cryptographic token that is used to validate a form
-
 DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
@@ -17,5 +14,5 @@ SECRET_KEY = 'my_precious'
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
-# The database uri
+# the database uri
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
